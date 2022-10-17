16fea0d39fc6656e287bb8666f8a98207c18feb3
al-jabir <aljabir098@gmail.com>
2022-10-17 14:49:37 +0600

    local data api

---
+++ pages/data.js
@@ -0,0 +1,34 @@
+const data = [
+  {
+    id: 1,
+    name: 'Sarah',
+    age: 24,
+    job: 'Frontend developer',
+  },
+  {
+    id: 2,
+    name: 'Emmah',
+    age: 34,
+    job: 'Mern Stack developer',
+  },
+  {
+    id: 3,
+    name: 'KNight',
+    age: 25,
+    job: 'Web3 developer',
+  },
+  {
+    id: 4,
+    name: 'Jack',
+    age: 14,
+    job: 'Backend developer',
+  },
+  {
+    id: 5,
+    name: 'Rythm',
+    age: 20,
+    job: 'Hacking developer',
+  },
+];
+
+export default data;
