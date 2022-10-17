d14275548c96a7092b9e4f9f71e98e075512d0d5
al-jabir <aljabir098@gmail.com>
2022-10-17 14:52:23 +0600

    done

--- pages/product/index.js
+++ pages/product/index.js
@@ -1,7 +1,18 @@
+import Link from 'next/link';
 import React from 'react';
 
 const index = () => {
-  return <div>This is my product page</div>;
+  return (
+    <div>
+      <h1>This is my product page</h1>
+      <Link href='/'>Home</Link>
+      <div>
+        <h3>Hello World</h3>
+        <h3>Hello Programmers</h3>
+        <h3>Hello Coders</h3>
+      </div>
+    </div>
+  );
 };
 
 export default index;
